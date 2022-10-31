427169dd7bb55232c2a218456aa1259dc183c304
FlavRomano <flavio@Flavios-MacBook-Air.local>
2022-10-31 12:35:03 +0100

    Ass 14 Oct Lab3

---
+++ LAB_RETI/Assignments/Ass_14_Oct/ConcurrentNetworkLogAnalysis/src/TimeNetworkLogComparator.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class TimeNetworkLogComparator {
+    public static class Task implements Runnable {
+        String[] args;
+        boolean concurrent;
+        long time;
+        public Task(String[] args, boolean concurrent) {
+            this.args = args;
+            this.concurrent = concurrent;
+        }
+        public void run() {
+            time = System.currentTimeMillis();
+            if (concurrent) {
+                ConcurrentNetworkLogPrinter.main(args);
+            } else {
+                NetworkLogPrinter.main(args);
+            }
+            long time2 = System.currentTimeMillis();
+            time = (time2 - time);
+        }
+    }
+    public static void main(String[] args) {
+        Task taskConcurrent = new Task(args, true);
+        Task taskSingleThread = new Task(args, false);
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 2; i++) {
+            service.submit(i == 0 ? taskConcurrent : taskSingleThread);
+        }
+        service.shutdown();
+        try {
+            service.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException ignored) {;}
+        System.out.format("Concurrent Network Log Analyser tempo: %d ms.%n", taskConcurrent.time);
+        System.out.format("Single Thread Network Log Analyser tempo: %d ms.%n", taskSingleThread.time);
+    }
+}
+// - A casa
+// Concurrent Network Log Analyser tempo:    63238 ms.     ~ 1.05  minuti
+// Single Thread Network Log Analyser tempo: 484048 ms.    ~ 8     minuti
+
+// - In dipartimento
+// Concurrent Network Log Analyser tempo:    13387 ms.     ~ 0.22   minuti
+// Single Thread Network Log Analyser tempo: 13421 ms.     ~ 0.22   minuti
