e2952a05d6bec7ca92bcc419595dc54f7d1c83c5
FlavRomano <flavio.romano92@gmail.com>
2023-02-14 12:19:48 +0100

    web scraping 14-feb lect.

--- LAB_SCRAPING/Code/test_beautifulsoup.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url: str = "https://www.repubblica.it/"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
-
-print(soup.findAll("p"))
